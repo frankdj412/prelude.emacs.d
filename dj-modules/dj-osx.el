fa32023280ce8621cc42203fd8f448f6134b240c
frankdj412 <frankdj412@gmail.com>
2015-02-03 22:56:52 +0800

    Add osx key shortcuts.

--- dj-modules/dj-osx.el
+++ dj-modules/dj-osx.el
@@ -8,7 +8,14 @@
   (setq mac-option-key-is-meta t)
   (setq mac-command-key-is-meta nil)
   (setq mac-command-modifier 'super)
-  (setq mac-option-modifier 'meta))
+  (setq mac-option-modifier 'meta)
+
+  (global-set-key (kbd "s-v") 'yank)
+  (global-set-key (kbd "s-c") 'kill-ring-save)
+  (global-set-key (kbd "s-x") 'kill-region)
+  (global-set-key (kbd "s-z") 'undo-tree-undo)
+  (global-set-key (kbd "s-s") 'save-buffer)
+  (global-set-key (kbd "s-Z") 'undo-tree-redo))
 
 (provide 'dj-osx)
 
